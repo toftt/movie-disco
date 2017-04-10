435669a07d1b835365671d5bb3aa12e927089351
Joachim Toft <c.joachim.toft@gmail.com>
2017-04-10 20:18:47 +0200

    Sets up dev environment

---
+++ webpack.config.js
@@ -0,0 +1,29 @@
+const path = require('path');
+module.exports = {
+  entry: './src/index.js',
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'bundle.js',
+  },
+  devServer: {
+    contentBase: [
+      path.join(__dirname, 'src'),
+      path.join(__dirname, 'dist'),
+    ],
+    compress: true,
+  },
+  module: {
+    rules: [
+      {
+        test: /.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['es2015', 'react'],
+          },
+        },
+      },
+    ],
+  },
+};
